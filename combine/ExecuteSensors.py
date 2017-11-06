b045e6eba0d0c14bf2338cdc68110dff3997c221
jcornel7 <jcornel7@users.noreply.github.com>
2017-11-06 12:53:51 -0500

    Desktop Builds

---
+++ combine/ExecuteSensors.py
@@ -0,0 +1,202 @@
+#Python Script to do following:
+# 1.Read sensor data
+# 2.Update OLED display with last read data
+# 3.Update LED Backpack with status letter if condition is met
+# 	a. Conditions for display is:
+#		- Humidity over 
+
+
+
+
+
+from Adafruit_BME280 import *
+
+sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+
+cdegrees = sensor.read_temperature()
+fdegrees = degrees*9/5+32
+pascals = sensor.read_pressure()
+hectopascals = pascals / 100
+humidity = sensor.read_humidity()
+
+print 'Temp      = {0:0.3f} deg F'.format(fdegrees)
+print 'Temp      = {0:0.3f} deg C'.format(cdegrees)
+print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+print 'Humidity  = {0:0.2f} %'.format(humidity)
+
+
+#----------------------------------------------------------------------------
+#LED Backpack
+
+import time
+
+from PIL import Image
+from PIL import ImageDraw
+
+from Adafruit_LED_Backpack import Matrix8x8
+
+
+# Create display instance on default I2C address (0x70) and bus number.
+display = Matrix8x8.Matrix8x8()
+
+# Alternatively, create a display with a specific I2C address and/or bus.
+# display = Matrix8x8.Matrix8x8(address=0x74, busnum=1)
+
+# Initialize the display. Must be called once before using the display.
+display.begin()
+
+# Run through each pixel individually and turn it on.
+for x in range(8):
+	for y in range(8):
+		# Clear the display buffer.
+		display.clear()
+		# Set pixel at position i, j to on.  To turn off a pixel set
+		# the last parameter to 0.
+		display.set_pixel(x, y, 1)
+		# Write the display buffer to the hardware.  This must be called to
+		# update the actual display LEDs.
+		display.write_display()
+		# Delay for half a second.
+		time.sleep(0.5)
+
+# Draw some shapes using the Python Imaging Library.
+
+# Clear the display buffer.
+display.clear()
+
+# First create an 8x8 1 bit color image.
+image = Image.new('1', (8, 8))
+
+# Then create a draw instance.
+draw = ImageDraw.Draw(image)
+
+# Draw a rectangle with colored outline
+draw.rectangle((0,0,7,7), outline=255, fill=0)
+
+# Draw an X with two lines.
+draw.line((1,1,6,6), fill=255)
+draw.line((1,6,6,1), fill=255)
+
+# Draw the image on the display buffer.
+display.set_image(image)
+
+# Draw the buffer to the display hardware.
+display.write_display()
+
+# See the SSD1306 library for more examples of using the Python Imaging Library
+# such as drawing text: https://github.com/adafruit/Adafruit_Python_SSD1306
+
+#------------------------------------------------------------------------------
+
+#OLED Status Screen
+
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import subprocess
+
+# Raspberry Pi pin configuration:
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# Beaglebone Black pin configuration:
+# RST = 'P9_12'
+# Note the following are only used with SPI:
+# DC = 'P9_15'
+# SPI_PORT = 1
+# SPI_DEVICE = 0
+
+# 128x32 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+# 128x64 display with hardware I2C:
+# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
+
+# Note you can change the I2C address by passing an i2c_address parameter like:
+# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
+
+# Alternatively you can specify an explicit I2C bus number, for example
+# with the 128x32 display you would use:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
+
+# 128x32 display with hardware SPI:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# 128x64 display with hardware SPI:
+# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# Alternatively you can specify a software SPI implementation by providing
+# digital GPIO pin numbers for all the required display pins.  For example
+# on a Raspberry Pi with the 128x32 display you might use:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('Minecraftia.ttf', 8)
+
+while True:
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+    cmd = "hostname -I | cut -d\' \' -f1"
+    IP = subprocess.check_output(cmd, shell = True )
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    CPU = subprocess.check_output(cmd, shell = True )
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+    MemUsage = subprocess.check_output(cmd, shell = True )
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+    Disk = subprocess.check_output(cmd, shell = True )
+
+    # Write two lines of text.
+
+    draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
+    draw.text((x, top+8),     str(CPU), font=font, fill=255)
+    draw.text((x, top+16),    str(MemUsage),  font=font, fill=255)
+    draw.text((x, top+25),    str(Disk),  font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.display()
+    time.sleep(.1)
