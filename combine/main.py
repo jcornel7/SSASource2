9daf701fc1e63437ff4766decd1534f55d59856f
jcornel7 <jcornel7@users.noreply.github.com>
2017-11-06 23:31:04 -0500

    buttoni

---
+++ combine/main.py
@@ -0,0 +1,8 @@
+#Main.py
+#This script executs the data gathering every 1 minute with location and timestamp.
+#This script takes the data and uploads it to #GCP  pub/sub
+#If there is network connectivity the LED 8x8 will diplay a Smile if not a frown
+#The stats per reading will be displayed on the OLED screen
+#The LED light will light everytime a reading is taken
+#The button is pushed everytime the fireplace is started
+
