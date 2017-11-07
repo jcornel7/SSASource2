9accd1ed27d9e8f4eb5997d404ca8b2c914a9dd9
jcornel7 <jcornel7@users.noreply.github.com>
2017-11-06 22:07:06 -0500

    scvzsv

--- combine/SensorDataDisplay.py
+++ combine/SensorDataDisplay.py
@@ -21,7 +21,14 @@ humidity = sensor.read_humidity()
 f = fdegrees
 c = cdegrees
 
+#Print to screen
+print 'Temp      = {0:0.3f} deg F'.format(fdegrees)
+print 'Temp      = {0:0.3f} deg C'.format(degrees)
+print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+print 'Humidity  = {0:0.2f} %'.format(humidity)
 
+
+#Print to OLED
 # Raspberry Pi pin configuration:
 RST = None     # on the PiOLED this pin isnt used
 # Note the following are only used with SPI:
@@ -72,8 +79,8 @@ while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
     # Write two lines of text.
-    draw.text((x, top),		  "%.2f" % float(fdegrees) + "F Degrees", font=font, fill=255)
-    draw.text((x, top+8),     "C:" + "%.2f" % float(cdegrees),  font=font, fill=255)
+    draw.text((x, top),		  "%.1f" % float(fdegrees) + " F Degrees", font=font, fill=255)
+    draw.text((x, top+8),     "%.1f" % float(cdegrees) + " C Degrees",  font=font, fill=255)
     draw.text((x, top+16),    "P:" + "%.2f" % float(hectopascals),  font=font, fill=255)
     draw.text((x, top+25),    "H:" + "%.2f" % float(humidity),  font=font, fill=255)
 
