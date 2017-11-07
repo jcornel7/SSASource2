4431d9685f9f662c9879e3940bf5e83990b030ca
jcornel7 <jcornel7@users.noreply.github.com>
2017-11-06 22:51:58 -0500

    acsfaff

---
+++ combine/SensorDataDisplay-Working.py
@@ -0,0 +1,92 @@
+
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from Adafruit_BME280 import *
+
+import subprocess
+
+sensor = BME280(t_mode=BME280_OSAMPLE_8, p_mode=BME280_OSAMPLE_8, h_mode=BME280_OSAMPLE_8)
+
+cdegrees = sensor.read_temperature()
+fdegrees = cdegrees*9/5+32
+pascals = sensor.read_pressure()
+hectopascals = pascals / 100
+humidity = sensor.read_humidity()
+f = fdegrees
+c = cdegrees
+
+#Print to screen
+print 'Temp      = {0:0.3f} deg F'.format(fdegrees)
+print 'Temp      = {0:0.3f} deg C'.format(cdegrees)
+print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
+print 'Humidity  = {0:0.2f} %'.format(humidity)
+
+
+#Print to OLED
+# Raspberry Pi pin configuration:
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# 128x32 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+#padding = -2
+padding = 0
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+
+# Load default font.
+#font = ImageFont.load_default()
+font = ImageFont.truetype('PrStart.ttf', 8)
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+# font = ImageFont.truetype('Minecraftia.ttf', 8)
+
+while True:
+
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    # Write two lines of text.
+    draw.text((x, top),		  "%.1f" % float(fdegrees) + " F Degrees", font=font, fill=255)
+    draw.text((x, top+8),     "%.1f" % float(cdegrees) + " C Degrees",  font=font, fill=255)
+    draw.text((x, top+16),    "%.2f" % float(hectopascals) + " hPa",  font=font, fill=255)
+    draw.text((x, top+25),    "%.2f" % float(humidity) + "% Humidity",  font=font, fill=255)
+
+    # Display image.
+    disp.image(image)
+    disp.display()
+    time.sleep(.1)
